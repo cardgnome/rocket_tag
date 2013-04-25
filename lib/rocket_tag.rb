3d7a1db0d07446c398b706faaf3508d33bc8a731
Chad McGimpsey <chad.mcgimpsey@gmail.com>
2013-04-24 23:08:00 -0600

    added functions to normalize tags

--- lib/rocket_tag.rb
+++ lib/rocket_tag.rb
@@ -13,6 +13,15 @@ module RocketTag
 
       tags = tags.dup
       tags = tags.map(&:downcase) if RocketTag.configuration.force_lowercase
+      tags = tags.map {|tag| tag.gsub(%r{\s+}, ' ')} if RocketTag.configuration.force_single_space
+      tags = tags.map do |tag|
+        RocketTag.configuration.invalid_words.each {|word| tag = tag.gsub(%r{(?<=(\s)|^)#{Regexp.escape(word)}?(?=($|\s))}, '') }
+        tag.strip
+      end unless RocketTag.configuration.invalid_words.empty?
+      tags = tags.map do |tag|
+        tag.gsub(%r{[^a-z0-9 #{Regexp.escape(RocketTag.configuration.valid_special_chars.join)}]}, '') # \-\'\#,; 
+      end unless RocketTag.configuration.valid_special_chars.empty?
+      tags = tags.map(&:singularize) if RocketTag.configuration.force_singular
       tags
     end
   end
